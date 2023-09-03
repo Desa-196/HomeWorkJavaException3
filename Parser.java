47453acbc6361572895e36421ea5e50bc620a11d
Денис Пономарев <deskin196@gmail.com>
2023-09-03 12:33:33 +0300

    Добавил генерацию исключения при неудачом преобразовании номера в классе Parser

--- Parser.java
+++ Parser.java
@@ -1,11 +1,16 @@
 import java.text.ParseException;
-import java.util.ArrayList;
 
 public class Parser {
-    public User parseText(String text) throws ParseException{
+    public User parseText(String text){
         String[] array = text.split(" ");
         if(array.length != 6) throw new ParserException("Некорректное количество данных!", null);
-        long telephone = Long.parseLong(array[4]);
+        long telephone;
+        try{
+            telephone = Long.parseLong(array[4]);
+        }
+        catch(NumberFormatException e){
+            throw new ParserException("Некорректный номер телефона! "+array[4], null);
+        }
         if(array[5].toCharArray().length > 1) throw new ParserException("Поле \"Пол\" содержит более одного символа!", null);
         return new User(array[0], array[1], array[2], array[3], telephone, array[5].toCharArray()[0]);
     }
